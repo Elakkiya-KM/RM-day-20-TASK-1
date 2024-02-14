6e1956816136e8149548b662d89acbf1c3b96e13
Elakkiya KM <elakkiyakmfuture@gmail.com>
2024-02-14 23:43:05 +0530

    UPDATED

---
+++ README.md
@@ -0,0 +1,15 @@
+# Road Map Day 20 TASK 1
+***********************
+## Random Quote Generator
+**************
+
+This Random Quote generator project completed using 
+
+ 1. HTML
+ 2. CSS
+ 3. JavaScript
+ 4. Bootstrap
+   
+   ***********************************
+
+Random Quote Generator DEMO LINK - [Click here](https://elakkiya-day20-task1-qoutegenerator.netlify.app/)
--- script.js
+++ script.js
@@ -1,56 +1,15 @@
-const quotes = [
-  {
-    quote: `"You only live once, but if you do it right, once is enough."`,
-    writer: `– Mae West`,
-  },
-  {
-    quote: `"If you want to live a happy life, tie it to a goal, not to people or things."`,
-    writer: `– Albert Einstein`,
-  },
-  {
-    quote: `"Never let the fear of striking out keep you from playing the game."`,
-    writer: `– Babe Ruth`,
-  },
-  {
-    quote: `"Your time is limited, so don’t waste it living someone else’s life."`,
-    writer: `– Steve Jobs`,
-  },
-  {
-    quote: `"In order to write about life first you must live it."`,
-    writer: `– Ernest Hemingway`,
-  },
-  {
-    quote: `"Life is not a problem to be solved, but a reality to be experienced."`,
-    writer: `– Soren Kierkegaard`,
-  },
-  {
-    quote: `"The unexamined life is not worth living."`,
-    writer: `– Socrates`,
-  },
-  {
-    quote: `"Turn your wounds into wisdom."`,
-    writer: `– Oprah Winfrey`,
-  },
-  {
-    quote: `"The purpose of our lives is to be happy."`,
-    writer: `- Dalai Lama`,
-  },
-  {
-    quote: `"Live for each second without hesitation."`,
-    writer: `- Elton John`,
-  },
-];
+const apiURL = "https://api.quotable.io/random";
+const quote = document.getElementById("quote");
+const author = document.getElementById("author");
 
-let btn = document.querySelector("#Qbtn");
+async function getQuote(url) {
+  const response = await fetch(apiURL);
+  var data = await response.json();
 
-let quote = document.querySelector(".quote");
+  console.log(data);
 
-let writer = document.querySelector(".writer");
+  quote.innerHTML = data.content;
+  author.innerHTML = data.author;
+}
 
-btn.addEventListener("click", function () {
-  let random = Math.floor(Math.random() * quotes.length);
-
-  quote.innerHTML = quotes[random].quote;
-
-  writer.innerHTML = quotes[random].writer;
-});
+getQuote(apiURL);
